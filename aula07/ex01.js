3711bd29d4ad351eafa2ecbaab339be21f727844
Cleber de Souza Oliveira <125616705+cleberdesouzaoliveira@users.noreply.github.com>
2023-06-27 20:03:11 -0300

    condições js

---
+++ aula07/ex01.js
@@ -0,0 +1,5 @@
+var  velocidade = 60
+console.log(`A velocidade do seu carro é ${velocidade} KM/h`)
+if( velocidade > 50) {
+    console.log('Voçê ultrapassou o limite da via e foi multado!')
+}
---
+++ aula07/ex02.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+if(pais == 'Brasil') {
+    console.log('Voçê é Brasileiro')
+}
+else {
+    console.log('Voçê é estrangeiro')
+}
